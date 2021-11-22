873b7b427bd5117ab67c6b56548cd4800254eb25
fahim-86 <fam.fahim@yahoo.com>
2021-11-23 04:29:37 +1100

    PEMDAS system explain as comments

--- Day 2/main.py
+++ Day 2/main.py
@@ -5,3 +5,11 @@ two_digit_number = input("Type a two digit number: ")
 firstNum = two_digit_number[0]
 secondNum = two_digit_number[1]
 print(int(firstNum) + int(secondNum))
+
+# !--- PEMDAS (L2R) ----!
+# Parentheses - ()
+# Exponents - x^2
+# Multiplication - *
+# Division - /
+# Addition - +
+# Subtraction "-"
