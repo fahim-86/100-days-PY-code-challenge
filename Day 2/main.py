62bee33c35bbd5cb14ef90bd28e1084fe58a2952
fahim-86 <fam.fahim@yahoo.com>
2021-12-26 16:44:48 +1100

    added code for Life Left in Weeks

--- Day 2/main.py
+++ Day 2/main.py
@@ -16,8 +16,18 @@
 
 
 # equation for calculate BMI = weight(KG) / height(Meter)^2
-height = input("enter your height in m: ")
+'''height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 
 BMI = int(weight) / float(height)**2
-print(int(BMI))
+print(int(BMI))'''
+
+# Your LIFE left in Weeks
+age = input("What is your current age? ")
+yearLeft = 90 - int(age)
+monthsLeft = yearLeft * 12
+weeksLeft = yearLeft * 52
+daysLeft = yearLeft * 365
+
+print(
+    f"You have {daysLeft} days, {weeksLeft} weeks and {monthsLeft} months left.")
