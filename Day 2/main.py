794ddeef7095f05812f8deecc0334c6205a5749e
fahim-86 <fam.fahim@yahoo.com>
2021-12-26 20:22:00 +1100

    Code for Tip calculator added

--- Day 2/main.py
+++ Day 2/main.py
@@ -23,11 +23,28 @@ BMI = int(weight) / float(height)**2
 print(int(BMI))'''
 
 # Your LIFE left in Weeks
-age = input("What is your current age? ")
+'''age = input("What is your current age? ")
 yearLeft = 90 - int(age)
 monthsLeft = yearLeft * 12
 weeksLeft = yearLeft * 52
 daysLeft = yearLeft * 365
 
-print(
-    f"You have {daysLeft} days, {weeksLeft} weeks and {monthsLeft} months left.")
+print(f"You have {daysLeft} days, {weeksLeft} weeks and {monthsLeft} months left.")'''
+
+# Tip Calculator
+print("Welcome to the tip calculator!")
+
+billAmount = input("What was the total bill? $")
+tipAmount = input("How much tip would you like to give? 10, 12, or 15? ")
+billSplit = input("How many people to split the bill? ")
+addedTip = 0.0
+
+if tipAmount == "12":
+    addedTip = float(billAmount) * 1.12
+elif tipAmount == "10":
+    addedTip = float(billAmount) * 1.1
+elif tipAmount == "15":
+    addedTip = float(billAmount) * 1.15
+
+splitedAmount = addedTip / int(billSplit)
+print(round(splitedAmount, 2))
